ecc6d8fb361ff428668d374c4ee80f7d4a737dff
David_Aanye <dmaanye@gmail.com>
2025-09-20 11:49:54 -0400

    infrs files update19

--- .github/terraform/ec2.tf
+++ .github/terraform/ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "terra" {
+# resource "aws_instance" "terra" {
 
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.prisubnet1.id
-  associate_public_ip_address = false
+#   ami                         = var.ami
+#   instance_type               = var.instance_type
+#   subnet_id                   = aws_subnet.prisubnet1.id
+#   associate_public_ip_address = false
    
 
  
-  tags = {
-    Name = var.server_name
-  }
-}
+#   tags = {
+#     Name = var.server_name
+#   }
+# }
