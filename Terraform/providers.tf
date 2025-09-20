eb6a6eb8307c85b61353c5195cf88cb99775740b
David_Aanye <dmaanye@gmail.com>
2025-09-20 19:47:32 -0400

    update

---
+++ Terraform/providers.tf
@@ -0,0 +1,21 @@
+ 
+ terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+
+  
+  backend "s3" {
+    bucket = "maabo-bucket"
+    key    = "environment/infrast/cloud/.tfstate"
+    region = "ca-central-1"
+  }
+}
+  
+provider "aws" {
+ region = "ca-central-1"
+  
+}
