8d0af7423e5069adce31ff80467dda807baf1f16
David_Aanye <dmaanye@gmail.com>
2025-09-20 18:18:41 -0400

    update

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
 
   backend "s3" {
     bucket = "maabo-bucket"
-    key    = "environment/infrast/cloud/tf.state"
+    key    = "environment/infrast/cloud/.tfstate"
     region = "ca-central-1"
   }
 
